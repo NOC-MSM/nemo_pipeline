7c4a18437afa513242dead7144b28f1ff3b78328
Ollie Tooth <oliver.tooth@noc.ac.uk>
2025-12-01 16:25:33 +0000

    Add eORCA1 ERA5v1 GSR, DS, FS & BSea pipeline configs and scripts.

---
+++ examples/NOC-NPD/eORCA1/submit_eORCA1_ERA5v1_BS_pipeline.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -euo pipefail
+# ================================================================
+# submit_eORCA1_ERA5v1_BS_pipeline.sh
+#
+# Description: Run NEMO Pipeline to extract the Barents Sea (BS)
+# hydrographic section from the eORCA1-ERA5v1 simulation.
+#
+# Created By: Ollie Tooth (oliver.tooth@noc.ac.uk)
+# Created On: 2025-12-01
+# ================================================================
+
+# -- Input arguments to NEMO Pipeline -- #
+# Define filepaths:
+config_file=eORCA1_ERA5v1_BS_config.toml
+log_file=eORCA1_ERA5v1_BS_pipeline.log
+
+# -- Python Environment -- #
+# Activate miniconda environment:
+source /home/otooth/miniconda3/etc/profile.d/conda.sh
+conda activate /dssgfs01/working/otooth/Software/conda_envs/env_nemo_cookbook
+
+# -- Run NEMO Pipeline CLI -- #
+# nemo_pipeline describe $config_file --log $log_file --input-pattern 2025
+# nemo_pipeline run $config_file --log $log_file --input-pattern 2025
+nemo_pipeline submit $config_file --log $log_file --submit
