409612b8d3bad815b5e9e830f01dc0cab064b369
Ollie Tooth <oliver.tooth@noc.ac.uk>
2025-12-01 13:28:06 +0000

    Update eORCA025 ERA5v1 - MOVE, SAMBA, RAPID, NOAC, OSNAP configs & scripts.

---
+++ examples/NOC-NPD/eORCA025/submit_eORCA025_ERA5v1_MOVE_pipeline.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -euo pipefail
+# ================================================================
+# submit_eORCA025_ERA5v1_MOVE_pipeline.sh
+#
+# Description: Run NEMO Pipeline to extract the Overturning in
+# the Meridional Overturning Experiment (MOVE) hydrographic section
+# from the eORCA025-ERA5v1 simulation.
+#
+# Created By: Ollie Tooth (oliver.tooth@noc.ac.uk)
+# Created On: 2025-11-01   
+# ================================================================
+
+# -- Input arguments to NEMO Pipeline -- #
+# Define filepaths:
+config_file=eORCA025_ERA5v1_MOVE_config.toml
+log_file=eORCA025_ERA5v1_MOVE_pipeline.log
+
+# -- Python Environment -- #
+# Activate miniconda environment:
+source /home/otooth/miniconda3/etc/profile.d/conda.sh
+conda activate /dssgfs01/working/otooth/Software/conda_envs/env_nemo_cookbook
+
+# -- Run NEMO Pipeline CLI -- #
+# nemo_pipeline describe $config_file --log $log_file --input-pattern 2025
+# nemo_pipeline run $config_file --log $log_file --input-pattern 2025
+nemo_pipeline submit $config_file --log $log_file --submit
