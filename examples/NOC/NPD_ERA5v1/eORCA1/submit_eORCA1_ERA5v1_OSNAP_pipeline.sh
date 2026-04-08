a70a578442a0c066c28e2751a19f84b294e9638e
Oliver Tooth <oliver.tooth@noc.ac.uk>
2026-04-08 15:07:07 +0100

    Update NOC NPD ERA5v1 - eORCA1 Examples.

--- examples/NOC/NPD_ERA5v1/eORCA1/submit_eORCA1_ERA5v1_OSNAP_pipeline.sh
+++ examples/NOC/NPD_ERA5v1/eORCA1/submit_eORCA1_ERA5v1_OSNAP_pipeline.sh
@@ -8,19 +8,15 @@ set -euo pipefail
 # the Subpolar North Atlantic Program (OSNAP) hydrographic section
 # from the eORCA1-ERA5v1 simulation.
 #
-# Created By: Ollie Tooth (oliver.tooth@noc.ac.uk)
-# Created On: 2025-11-01   
+# Virtual Environment: env_nemo
+#
+# Created By: Ollie Tooth (oliver.tooth@noc.ac.uk) 
 # ================================================================
 
 # -- Input arguments to NEMO Pipeline -- #
 # Define filepaths:
 config_file=eORCA1_ERA5v1_OSNAP_config.toml
 log_file=eORCA1_ERA5v1_OSNAP_pipeline.log
-
-# -- Python Environment -- #
-# Activate miniconda environment:
-source /home/otooth/miniconda3/etc/profile.d/conda.sh
-conda activate /dssgfs01/working/otooth/Software/conda_envs/env_nemo_cookbook
 
 # -- Run NEMO Pipeline CLI -- #
 # nemo_pipeline describe $config_file --log $log_file --input-pattern 2025
